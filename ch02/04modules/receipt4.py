55084661255ca04c07c39e61442da6a85330d99e
iruma-tea <shusuke.think.pad@gmail.com>
2024-02-10 21:40:03 +0900

    2.1.2 importの種類の学習完了

---
+++ ch02/04modules/receipt4.py
@@ -0,0 +1,15 @@
+# @@range_begin(list1)  # ←この行は無視してください。書籍の本文に引用するためのものです。
+# ch02/04modules/receipt4.py
+from sales_tax import *
+
+
+def print_receipt():
+    total = 1000.0
+    state = 'MI'
+    grand_total = add_local_millage_tax(add_city_tax(
+        add_state_tax(add_sales_tax(total, state))))
+    print(f'合計: {grand_total}')
+# @@range_end(list1)  # ←この行は無視してください。書籍の本文に引用するためのものです。
+
+
+print_receipt()
