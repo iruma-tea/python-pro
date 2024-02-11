3bcf36b1d2526848dc51bdec4d947779362cbc67
iruma-tea <shusuke.think.pad@gmail.com>
2024-02-11 20:07:10 +0900

    2.2 Pythonにおける分離の階層/2.2.1関数の学習開始

---
+++ ch02/05time/dateboth2.py
@@ -0,0 +1,9 @@
+# @@range_begin(list1)  # ←この行は無視してください。書籍の本文に引用するためのものです。
+import time
+import datetime
+
+now = time.time()
+midnight = datetime.time()
+# @@range_end(list1)  # ←この行は無視してください。書籍の本文に引用するためのものです。
+print('Unix時間:', now)
+print('デフォルトの時刻:', midnight)
---
+++ ch02/06datemylib/time.py
@@ -0,0 +1,8 @@
+# @@range_begin(list1)  # ←この行は無視してください。書籍の本文に引用するためのものです。
+import datetime
+
+from mycoolibrary import datetime as cooldatetime
+# @@range_end(list1)  # ←この行は無視してください。書籍の本文に引用するためのものです。
+
+print(cooldatetime())
+print(datetime.time())
