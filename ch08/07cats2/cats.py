6ceb956e563363c1b738934f66dc3eb91efafe39
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-19 20:12:50 +0900

    抽象クラスに自身にメソッドを追加

---
+++ ch08/07cats2/cats.py
@@ -0,0 +1,27 @@
+class BigCat:
+    def eats(self):
+        return ['齧歯動物']
+
+
+class Lion(BigCat):
+    def eats(self):
+        return super().eats() + ['ヌー']
+
+
+class Tiger(BigCat):
+    def eats(self):
+        return super().eats() + ['水牛']
+
+
+class Liger(Lion, Tiger):
+    def eats(self):
+        return super().eats() + ['兎', '牛', '豚', '鶏']
+
+
+if __name__ == '__main__':
+    lion = Lion()
+    print('ライオンが食べるもの：', lion.eats())
+    tiger = Tiger()
+    print('トラが食べるもの：', tiger.eats())
+    liger = Liger()
+    print('ライガーが食べるもの：', liger.eats())
