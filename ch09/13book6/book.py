5310398f646ba5e6b1ec1208a0b19699f583509b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-20 16:57:23 +0900

    ch09の学習完了

---
+++ ch09/13book6/book.py
@@ -0,0 +1,47 @@
+import warnings
+
+
+class Author:
+    def __init__(self, author_data):
+        self.first_name = author_data['first_name']
+        self.last_name = author_data['last_name']
+
+    @property
+    def for_display(self):
+        return f'{self.first_name} {self.last_name}'
+
+    @property
+    def for_citation(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
+class Book:
+    def __init__(self, data):
+        self.title = data['title']
+        self.subtitle = data['subtitle']
+        self.author_data = data['author']
+        self.author = Author(self.author_data)
+
+    @property
+    def author_for_display(self):
+        warnings.warn('book.author.for_displayを利用してください', DeprecationWarning)
+        return self.author.for_display
+
+    @property
+    def author_for_citation(self):
+        warnings.warn('book.author.for_citationを利用してください', DeprecationWarning)
+        return self.author.for_citation
+
+
+book = Book({
+    'title': 'Brillo-iant',
+    'subtitle': 'The pad that changed everything',
+    'author': {
+        'first_name': 'Rusty',
+        'last_name': 'Potts',
+    }
+})
+
+
+print(book.author_for_display)
+print(book.author_for_citation)
